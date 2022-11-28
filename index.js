1f136c7178b96b2348cf69dfeaf6e5c2ab13e3e5
Kellymugambi <kellymgambi@.com>
2022-11-28 03:23:34 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(guys, message){
+    let messages = []
+    for (const guy of guys) {
+        messages.push (`Thank you, ${guy}, for the wonderful ${message} gift!`)
+    }
+    return messages
+}
+function countDown (y){
+    for(let i =y; i>=0; i--){
+        console.log(i)
+    }
+}
